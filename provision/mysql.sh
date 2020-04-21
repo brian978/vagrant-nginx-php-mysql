d328f3cefb6ed57335a04a41eed76ac97d8048c2
Andrei Dincescu <brian978@users.noreply.github.com>
2020-04-21 11:33:52 +0300

    Updated MySQL install & reset script

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -5,8 +5,8 @@ dnf module reset -y mysql
 dnf module disable -y mysql
 
 # Get the MySQL official RPM
-curl -L https://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/mysql57-community-release-el7-10.noarch.rpm -o /tmp/mysql57-community-release-el7-10.noarch.rpm
-rpm -ivh /tmp/mysql57-community-release-el7-10.noarch.rpm
+rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql
+rpm -Uvh https://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/mysql57-community-release-el7-10.noarch.rpm
 
 # Install & enable the server
 dnf install -y mysql-community-server
